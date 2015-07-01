64dd9835fc8ffe2ee6eee8d781523c29315e8b58
wf <willfreyman@gmail.com>
2015-07-01 10:13:33 -0700

    handling exceptions, printing status, other stuff

--- species_data_scraper.py
+++ species_data_scraper.py
@@ -4,15 +4,27 @@
 import codecs
 import requests
 import time
+import sys
 
-links = ['http://www2.darwin.edu.ar/Proyectos/FloraArgentina/DetalleEspecie.asp?forma=&variedad=&subespecie=&especie=grandifolium&genero=Abutilon&espcod=15928']
+#links = ['http://www2.darwin.edu.ar/Proyectos/FloraArgentina/DetalleEspecie.asp?forma=&variedad=&subespecie=&especie=grandifolium&genero=Abutilon&espcod=15928',
+#         'http://www2.darwin.edu.ar/Proyectos/FloraArgentina/DetalleEspecie.asp?forma=&variedad=&subespecie=&especie=weddelliana&genero=Aa&espcod=23894']
 
-output_file = "test.csv"
+links = []
+num_links = 0
+with open('species_links.txt') as f:
+    for line in f:
+        links.append(line)
+        num_links += 1
+
+output_file = "darwinion_accepted_species.csv"
 file = codecs.open(output_file, "w", "utf-8")
 file.write(u'"Familia","Género","Especie","Sigla sp.","Subespecie","Sigla ssp.","Variedad","Sigla var.","Forma","Sigla f.","Publicado en","Volumen","Páginas","Año","Hábito","Status","Elevación (m s.m.)","Distribución Argentina","Distribución Misiones","Paises limítrofes","Distribución Brasil","Distribución Chile","Distribución Paraguay","Distribución Uruguay","Notas","Sinónimos"\n')
 file.close()
 
 def get_content(key, source):
+    '''
+    Parse a keyword from the html source
+    '''
     if source.find(key) != -1:
         start = source.find(key)
         end = source.find('</td>', start + len(key))
@@ -21,6 +33,9 @@ def get_content(key, source):
         return ''
 
 def get_synonyms(source):
+    '''
+    Parse the synonyms
+    '''
     key = u'Sinónimos: </strong>'
     if source.find(key) != -1:
         start = source.find(key)
@@ -34,6 +49,7 @@ def get_synonyms(source):
             syn = synonym[end_anchor:].strip()
             syn = syn.replace(', hom. illeg.', '')
             syn = syn.replace(',', '')
+            syn = syn.replace('  ', ' ')
             if syn.strip() != '':
                 synonyms_list_no_anchors.append(syn)
         synonyms = u','.join(synonyms_list_no_anchors)
@@ -41,29 +57,59 @@ def get_synonyms(source):
     else:
         return ''
 
+def prep_genbank_mining(row):
+    '''
+    Generate a list of species for MatrixMaker genbank mining
+    '''
+    if row[21] != '':
+        accepted_binomial = u' '.join((row[1], row[2])) 
+        file = codecs.open('gb_species.csv', 'a', 'utf-8')
+        file.write(u','.join((accepted_binomial, row[25])).replace('"',''))
+        file.close()
 
+
+def get_data(link):
+    '''
+    Scrape the data for this species.
+    '''
+    try:
+        page = requests.get(link)
+        page.encoding = 'utf-8'
+        source = page.text
+        keys = [u'Familia</td><td width = "65%">',u'Género</td><td width="65%">',u'Especie</td><td width = "65%">',u'Sigla sp.</td><td width = "65%">',
+                u'Subespecie</td><td width = "65%">',u'Sigla ssp.</td><td width = "65%">',u'Variedad</td><td width = "65%">',u'Sigla var.</td><td width = "65%">',
+                u'Forma</td><td width = "65%">',
+                u'Sigla f.</td><td width = "65%">',u'Publicado en</td><td width = "65%">',u'Volumen</td><td width = "65%">',u'Páginas</td><td width = "65%">',
+                u'Año</td><td width = "65%">',
+                u'Hábito</td><td width = "65%">',u'Status</td><td width = "65%">',u'Elevación (m s.m.)</td><td width = "65%">',u'Distribución Argentina</td><td width = "65%">',
+                u'Distribución Misiones</td><td width = "65%" class=cuerpo>',u'Paises limítrofes</td><td width = "65%">',u'Distribución Brasil</td><td width = "75%" class=cuerpo>',
+                u'Distribución Chile</td><td width = "65%" class=cuerpo>',u'Distribución Paraguay</td><td width = "65%" class=cuerpo>',
+                u'Distribución Uruguay</td><td width = "65%" class=cuerpo>',u'Notas</td><td width = "65%">']
+        row = []
+        for key in keys:
+            row.append(get_content(key, source))
+        row.append(get_synonyms(source))
+        row.append(u'\n')
+
+        prep_genbank_mining(row) 
+        file = codecs.open(output_file, "a", "utf-8")
+        file.write(u','.join(row)) 
+        file.close()
+        # dont overload server
+        time.sleep(0.2)
+
+    except Exception as e:
+        # need to retry
+        time.sleep(0.5)
+        get_data(link)
+
+
+i = 1
 for link in links:
-    page = requests.get(link)
-    page.encoding = 'utf-8'
-    source = page.text
-    keys = [u'Familia</td><td width = "65%">',u'Género</td><td width="65%">',u'Especie</td><td width = "65%">',u'Sigla sp.</td><td width = "65%">',
-            u'Subespecie</td><td width = "65%">',u'Sigla ssp.</td><td width = "65%">',u'Variedad</td><td width = "65%">',u'Sigla var.</td><td width = "65%">',
-            u'Forma</td><td width = "65%">',
-            u'Sigla f.</td><td width = "65%">',u'Publicado en</td><td width = "65%">',u'Volumen</td><td width = "65%">',u'Páginas</td><td width = "65%">',
-            u'Año</td><td width = "65%">',
-            u'Hábito</td><td width = "65%">',u'Status</td><td width = "65%">',u'Elevación (m s.m.)</td><td width = "65%">',u'Distribución Argentina</td><td width = "65%">',
-            u'Distribución Misiones</td><td width = "65%" class=cuerpo>',u'Paises limítrofes</td><td width = "65%">',u'Distribución Brasil</td><td width = "75%" class=cuerpo>',
-            u'Distribución Chile</td><td width = "65%" class=cuerpo>',u'Distribución Paraguay</td><td width = "65%" class=cuerpo>',
-            u'Distribución Uruguay</td><td width = "65%" class=cuerpo>',u'Notas</td><td width = "65%">']
-    row = []
-    for key in keys:
-        row.append(get_content(key, source))
-    row.append(get_synonyms(source))
-    row.append(u'\n')
-    
-    file = codecs.open(output_file, "a", "utf-8")
-    file.write(u','.join(row)) 
-    file.close()
-
-    #print(u''.join(("http://www2.darwin.edu.ar/Proyectos/FloraArgentina/DetalleEspecie.asp?", link[:end])).encode('utf-8').strip())
-    time.sleep(0.2)
+    percent = str(round(100 * i/float(num_links), 2))
+    sys.stdout.write('\r' + 'Completed: ' + str(i) + '/' + str(num_links) + ' (' + percent + '%)')
+    sys.stdout.flush()
+    get_data(link)
+    i += 1
+    #if i == 10:
+    #    sys.exit(0)
